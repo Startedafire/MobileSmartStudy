d97cac864cb0b4aa7460a81b3df137164b810214
startedafire <karljosephgaitera@gmail.com>
2025-10-05 17:56:10 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/main_tabs.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   await Supabase.initialize(
-    url: 'https://bhvisxadsibfeymwnmdx.supabase.co', // replace with your URL
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJodmlzeGFkc2liZmV5bXdubWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMTk3ODMsImV4cCI6MjA3NDg5NTc4M30.qgtqDS2cZ6028neWDuCxm674H9NslS04OjFmhEaLQJw',          // replace with anon key
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(const MyApp());
